ecc29d06c2fa31ed789ec87414c45c0a3ab6c3d9
MeRichard123 <rcoric03@gmail.com>
2019-10-30 18:32:41 +0000

    rebrand

---
+++ app.js
@@ -0,0 +1,12 @@
+// window.addEventListener("load", () => {
+//   alert("load");
+//   const button = document.querySelectorAll("a.solo");
+//   button.addEventListener("click", () => {
+//     console.log("clicked");
+//     alert("hello");
+//   });
+// });
+
+show = function() {
+  alert("Website Created with skills learned from this amazing webite");
+};
