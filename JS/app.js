f2d6183c1d74db3be33d5fbeb5443476dffa4d2e
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-06-16 18:20:05 +0100

    Name switch script

--- JS/app.js
+++ JS/app.js
@@ -7,6 +7,12 @@
 //   });
 // });
 
-show = function() {
-  alert("Website Created with skills learned from this amazing webite");
+const TeamName = "Equifox";
+
+show = function () {
+  alert("Website Created with skills learned from this amazing website");
 };
+const teamName = document.querySelectorAll("[data-team]");
+teamName.forEach((name) => {
+  name.innerHTML = TeamName;
+});
