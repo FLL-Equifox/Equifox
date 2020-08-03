f583ab3cf6802c64e2906609633a8d675a4a2a2e
Richard <51269862+MeRichard123@users.noreply.github.com>
2020-08-03 11:37:18 +0100

    footer update

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,5 @@
 const TeamName = "Equifox";
 
-show = function () {
-  alert("Website Created with skills learned from this amazing website");
-};
 const teamName = document.querySelectorAll("[data-team]");
 teamName.forEach((name) => {
   name.innerHTML = TeamName;
